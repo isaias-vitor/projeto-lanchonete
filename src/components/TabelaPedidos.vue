9f55ce7cea9d6a2e962fe20a704aab7eea8afae7
Isaias Vitor <isaiasvitorslva@gmail.com>
2024-06-25 15:38:28 -0300

    Adicionada lógica que puxa dos pedidos do banco de dados a insere na tabela

--- src/components/TabelaPedidos.vue
+++ src/components/TabelaPedidos.vue
@@ -1,52 +1,64 @@
 <template>
-  <div class="p-5">
-    <table class="table text-center">
-        <thead>
-            <tr>
-                <th scope="col" class="text-center">#</th>
-                <th scope="col" class="text-center">Cliente</th>
-                <th scope="col" class="text-center">Pão</th>
-                <th scope="col" class="text-center">Carne</th>
-                <th scope="col" class="text-center">Opcionais</th>
-                <th scope="col" class="text-center">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row">1</td>
-                <td >Vitor</td>
-                <td>Parmesão e orégano</td>
-                <td>Maminha</td>
-                <td>
-                    <ul>
-                        <li>Bacon</li>
-                        <li>Cebola roxa</li>
-                        <li>Cheddar</li>
-                        <li>Tomate</li>
-                    </ul>
-                </td>
-                <td>
-                    <div class="d-flex justify-content-around">
-                        <div class="col-5 d-inline-block">
-                            <select class="form-select" name="" id="">
-                                <option value="" select>Selecione</option>
-                                <option value="concluido">Concluído</option>
-                            </select>
+    <div class="p-5">
+        <table class="table text-center">
+            <thead>
+                <tr>
+                    <th scope="col" class="text-center">#</th>
+                    <th scope="col" class="text-center">Cliente</th>
+                    <th scope="col" class="text-center">Pão</th>
+                    <th scope="col" class="text-center">Carne</th>
+                    <th scope="col" class="text-center">Opcionais</th>
+                    <th scope="col" class="text-center">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="burger in burgers" :key="burger.id">
+                    <td scope="row"> {{ burger.id }}</td>
+                    <td>{{ burger.nome }}</td>
+                    <td> - </td>
+                    <td>{{burger.carne}}</td>
+                    <td>
+                        <ul>
+                            <li v-for="(opcionais, index) in burger.opcionais" :key="index">{{opcionais}}</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-evenly">
+                            <div class="d-inline-block">
+                                <select class="form-select" name="" id="">
+                                    <option value="" select>Selecione o status do hamburger</option>
+                                    <option value="concluido">Concluído</option>
+                                </select>
+                            </div>
+                            <button class="btn cancela">Cancelar</button>
                         </div>
-                        <button class="btn cancela">CANCELAR</button>
-                    </div>
-                </td>
-            </tr>
-            
-            
-        </tbody>
+                    </td>
+                </tr>            
+            </tbody>
         </table>
-  </div>  
+    </div>  
 </template>
 
 <script>
 export default{
     name: 'TabelaPedidos',
+    data(){
+        return{
+            burgers: null,
+            burger_id: null,
+            status_burger: null,
+        }
+    },
+    methods: {
+        async getBurgers(){
+            const req = await fetch('http://localhost:3000/burgers');
+            const data = await req.json();
+            this.burgers = data;
+        }
+    },
+    mounted(){
+        this.getBurgers();
+    }
 }
 </script>
 
